a24a2a0278ba859b4948f97f7faa2d3c44465fa8
rayccz <ray-ccz@hotmail.com.tw>
2019-07-21 21:12:52 -0700

    add icon

---
+++ src/components/SlideBar.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import EventCard from './EventCard';
+import soccer from './../soccer.png';
+import './SlideBar.css';
+
+import { DefaultButton, PrimaryButton, ChoiceGroup, Panel, PanelType, Fabric } from 'office-ui-fabric-react';
+
+export default class SlideBar extends React.Component {
+    constructor(){
+        super();
+
+        this._showPanel = () => {
+            this.setState({ showPanel: true });
+        };
+        this._hidePanel = () => {
+            this.setState({ showPanel: false });
+        };
+        this.state={showPanel:false};
+    }
+    render() {
+        return (
+        <div className="SlideBar-top">
+            <DefaultButton
+                className="NavButton"
+                iconProps={{ iconName: 'GlobalNavButton' }}
+                onClick={this._showPanel}
+            />
+            <Panel 
+                className="SlideBar-main"
+                isOpen={this.state.showPanel}
+                type={PanelType.smallFixedFar}
+                onDismiss={this._hidePanel}
+                isLightDismiss={true}
+                hasCloseButton={false}
+                headerText="Your Events"
+                //onRenderFooterContent={this._onRenderFooterContent}
+                closeButtonAriaLabel="Close">
+                <div>
+                {this.props.events.map(e=><EventCard title={e.title} eventInfo={e.eventInfo}/>)}
+                </div>
+            </Panel>
+        </div>
+        );
+    }
+
+    /*
+    private _onRenderFooterContent = () => {
+        return (
+        <div>
+            <PrimaryButton onClick={this._hidePanel} style={{ marginRight: '8px' }}>
+            Save
+            </PrimaryButton>
+            <DefaultButton onClick={this._showPanel}>Cancel</DefaultButton>
+        </div>
+        );
+    };
+    */
+
+    
+}
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import EventCard from './components/EventCard'
 import {initializeIcons} from 'office-ui-fabric-react';
 import soccer from './soccer.png';
 
+import SlideBar from './components/SlideBar';
+
 initializeIcons();
 const eventInfoSample={
   location:"1 Microsoft Way, Redmond, WA",
@@ -11,8 +13,18 @@ const eventInfoSample={
   time:"5:25pm"
 }
 
+/*
 ReactDOM.render(
     <EventCard title="Sample Event" eventInfo={eventInfoSample} img={soccer}/>,
   document.getElementById('root')
 );
+*/
+const eventList=[...Array(5).keys()].map(i=>({title:"test"+i, eventInfo:eventInfoSample}));
+
+ReactDOM.render(
+  <div>
+    <SlideBar events={eventList}/>
+  </div>,
+  document.getElementById('root')
+)
 
