8d74c0dcfdbbd7e0551d09c3e12fd1bf29758637
rayccz <ray-ccz@hotmail.com.tw>
2019-07-23 00:28:57 -0700

    add random color

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import InterestEventPickerState from './InterestEventPicker'
 import './Profile.css';
 
 
+
 export default class Profile extends React.Component {
+    constructor(){
+        super();
+        this.seed = 1;
+        this.random = () => {
+            var x = Math.sin(this.seed++) * 10000;
+            return x - Math.floor(x);
+        }
+        this.generateColor = () =>{
+            return '#' +  this.random().toString(16).substr(-6);
+        }
+    }
     render() {
         return (
             <div className="profile_main">
@@ -13,16 +26,18 @@ export default class Profile extends React.Component {
                         <div className="blank"></div>
                         <h2 className="interest_tag_name">Interest Tag:</h2>
                         <div className="interest_tag_main">
-                            <table className="item">
-                                <tr>
-                                <td className="tag_text"> Sports 1</td>
-                                <td className="tag_text"> Video Game fgdfgdf1</td>
-                                </tr>
-                                <tr>
-                                <td className="tag_text"> Sports </td>
-                                <td className="tag_text"> Video Game </td>
-                                </tr>
-                            </table>
+                            <div className="item">
+                                <div className="tag_text" style={{background:this.generateColor()}}>#Sport</div>
+                                <div className="tag_text" style={{background:this.generateColor()}}>#Movie</div>
+                                <div className="tag_text" style={{background:this.generateColor()}}>#Friday Night</div>
+                                <div className="tag_text" style={{background:this.generateColor()}}>#Video Game</div>
+                                <div className="tag_text" style={{background:this.generateColor()}}>#Racing</div>
+                                <div className="tag_text" style={{background:this.generateColor()}}>#Pro Club</div>
+                                <div className="tag_text" style={{background:this.generateColor()}}>#Gym</div>
+                                <div className="tag_text" style={{background:this.generateColor()}}>#Pub</div>
+                                <div className="tag_text" style={{background:this.generateColor()}}>#Poker</div>
+                                <div className="tag_text" style={{background:this.generateColor()}}>#Dinner</div>
+                            </div>
                         </div>
                     </div>
                 </div>
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import EventCard from './components/EventCard'
 import Profile from './components/Profile'
 import {initializeIcons} from 'office-ui-fabric-react';
 import soccer from './soccer.png';
-import user from './user.jpg';
+import user from './user2.jpg';
 import EventWall from './components/EventWall'
 import './index.css';
 
@@ -27,7 +27,7 @@ const InterestList=["Tag1", "Tag2", "Tag3"]
 
 ReactDOM.render(
   <div>
-      <Profile image={user} name={"Wenynag Chao"} Interest_tag={InterestList}/>
+      <Profile image={user} name={"Mr Mario"} Interest_tag={InterestList}/>
   </div>,
 
   document.getElementById('root')
